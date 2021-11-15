79c40b55402723680a3971231a48b3fb9748f1ec
parvezaman <emailtoamanullah@gmail.com>
2021-11-15 13:59:57 +0600

    sending request to make a user admin

--- src/pages/DashboardPage/MakeAdmin/MakeAdmin.js
+++ src/pages/DashboardPage/MakeAdmin/MakeAdmin.js
@@ -4,24 +4,36 @@ import Button from '@mui/material/Button';
 
 const MakeAdmin = () => {
     const [email, setEmail] = useState('');
-    const handleOnBlur = e =>{
+    const handleOnBlur = e => {
         setEmail(e.target.value);
     }
     const handleMakeAnAdmin = (e) => {
-
+        const user = { email };
+        console.log(user);
+        fetch('http://localhost:5000/users/admin', {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
         e.preventDefault();
     }
     return (
         <div className="text-center">
             <h3 className="mb-5">Let us be an admin</h3>
             <form onSubmit={handleMakeAnAdmin}>
-                <TextField 
-                id="standard-basic" 
-                type='email'
-                className="me-5"
-                onBlur={handleOnBlur}
-                label="email" 
-                variant="outlined" 
+                <TextField
+                    id="standard-basic"
+                    type='email'
+                    className="me-5"
+                    onBlur={handleOnBlur}
+                    label="email"
+                    variant="outlined"
                 />
                 <Button type='submit' variant="outlined">Make Admin</Button>
             </form>
